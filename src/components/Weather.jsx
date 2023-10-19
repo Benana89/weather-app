92660edc6dc888c6df9652bdd6efa8abe7023325
Benana89 <48176330+Benana89@users.noreply.github.com>
2023-10-20 00:25:26 +0800

    set re search aand delete

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,7 @@
 // Weather.js
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import cloud from "../assets/cloud.png";
+import sun from "../assets/sun.png";
 
 const Weather = () => {
   const [city, setCity] = useState("");
@@ -8,10 +10,20 @@ const Weather = () => {
   const [error, setError] = useState(null);
   const [searchHistory, setSearchHistory] = useState([]);
 
+  const flag = useRef(false);
+
+  useEffect(() => {
+    if (flag.current) {
+      handleFetchWeather();
+      flag.current = false;
+    }
+  }, [city, country]);
+
   const apiKey = "17bbda84c541166d22dd2daf6185f992"; // Replace with your API key
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`;
 
   const handleFetchWeather = () => {
+    setError(null);
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
@@ -33,9 +45,39 @@ const Weather = () => {
       });
   };
 
+  const renderWeatherImage = () => {
+    if (weatherData) {
+      const weatherCondition = weatherData.weather[0].main.toLowerCase();
+      if (weatherCondition === "rain") {
+        return <img className="image-style" src={cloud} />; // Cloud image for rain
+      } else {
+        return <img className="image-style" src={sun}></img>; // Sun image for other conditions
+      }
+    }
+    return null;
+  };
+
+  const handleDeleteSearch = (index) => {
+    // Remove the selected search history item by its index
+    const updatedSearchHistory = [...searchHistory];
+    updatedSearchHistory.splice(index, 1);
+    setSearchHistory(updatedSearchHistory);
+  };
+
+  const handleReselectSearch = (index) => {
+    // Retrieve and re-search the selected search history item
+    const selectedSearch = searchHistory[index];
+    const [selectedCity, selectedCountry] = selectedSearch.split(", ");
+
+    setCity(selectedCity);
+    setCountry(selectedCountry);
+
+    flag.current = true;
+  };
+
   return (
     <div className="overall-area">
-      <div>
+      <div className="input-area">
         <input
           type="text"
           placeholder="City"
@@ -49,10 +91,11 @@ const Weather = () => {
           onChange={(e) => setCountry(e.target.value)}
         />
         <button onClick={handleFetchWeather}>Get Weather</button>
+        {error && <p>{error}</p>}
       </div>
-      {error && <p>{error}</p>}
 
       <div className="weather-area">
+        <div className="weather-placeholder"> {renderWeatherImage()}</div>
         {weatherData && (
           <div className="header-left">
             <div>Today's Weather</div>
@@ -75,6 +118,15 @@ const Weather = () => {
               {searchHistory.map((search, index) => (
                 <div className="search-item" key={index}>
                   {search}
+
+                  <div>
+                    <button onClick={() => handleReselectSearch(index)}>
+                      Re-Search
+                    </button>
+                    <button onClick={() => handleDeleteSearch(index)}>
+                      Delete
+                    </button>
+                  </div>
                 </div>
               ))}
             </div>
